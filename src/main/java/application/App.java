54b9c4c99eaf0c4f217b977498a645e3e40e9ab9
inesb <bourhim.ines@gmail.com>
2023-02-10 14:41:54 +0100

    Réintégration de l'interface graphique pour la page d'intégration

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,17 +1,18 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Hello world!
  *
  */
-public class App {
-    public static ShiFuMi scene;
+public class App{
     public static void main( String[] args )
     {
         Application.launch(ShiFuMi.class, args);
-
-
     }
 }
