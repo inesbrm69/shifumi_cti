b0a9cebef9bafe88a9964f9e2467ba6e0f3e4dfa
inesb <bourhim.ines@gmail.com>
2023-02-10 15:29:36 +0100

    Test du bouton qui renvoie vers une autre page interface graphique

---
+++ src/main/java/application/ShiFuMiController.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ShiFuMiController {
+    @FXML
+
+    public void nextPage(ActionEvent e) throws IOException {
+        AnchorPane gamePage = FXMLLoader.load(Jeu.class.getResource("PageGame.fxml"));
+        Scene scene = new Scene(gamePage);
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
