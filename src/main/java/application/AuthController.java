5bd9ac09c3fbd1676915b936d0d5cdb002c0aa63
BKC3CE <celia.pallier@hotmail.com>
2023-02-16 07:09:13 +0100

    ajout méthode pour sign in, plus choix perso

--- src/main/java/application/AuthController.java
+++ src/main/java/application/AuthController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -43,13 +45,95 @@ public class AuthController {
     @FXML
     private Label errorMsg;
 
+    private int choicePerso = 0;
+
+    public TextField getUsername() {
+        return username;
+    }
+
+    public PasswordField getPassword() {
+        return password;
+    }
+
+    public Button getLogIn() {
+        return logIn;
+    }
+
+    public Button getSignIn() {
+        return signIn;
+    }
+
+    public Button getImageZero() {
+        return imageZero;
+    }
+
+    public Button getImageOne() {
+        return imageOne;
+    }
+
+    public Button getImageTwo() {
+        return imageTwo;
+    }
+
+    public Button getImageThree() {
+        return imageThree;
+    }
+
+    public Label getErrorMsg() {
+        return errorMsg;
+    }
+
+    public int getChoicePerso() {
+        return choicePerso;
+    }
+
+    public void setUsername(TextField username) {
+        this.username = username;
+    }
+
+    public void setPassword(PasswordField password) {
+        this.password = password;
+    }
+
+    public void setLogIn(Button logIn) {
+        this.logIn = logIn;
+    }
+
+    public void setSignIn(Button signIn) {
+        this.signIn = signIn;
+    }
+
+    public void setImageZero(Button imageZero) {
+        this.imageZero = imageZero;
+    }
+
+    public void setImageOne(Button imageOne) {
+        this.imageOne = imageOne;
+    }
+
+    public void setImageTwo(Button imageTwo) {
+        this.imageTwo = imageTwo;
+    }
+
+    public void setImageThree(Button imageThree) {
+        this.imageThree = imageThree;
+    }
+
+    public void setErrorMsg(Label errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public void setChoicePerso(int choicePerso) {
+        this.choicePerso = choicePerso;
+    }
+
     @FXML
     public void userLogIn(ActionEvent e) throws IOException {
-        AnchorPane authPage = FXMLLoader.load(Jeu.class.getResource("pageAuthentification.fxml"));
-        Scene scene = new Scene(authPage);
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        //AnchorPane authPage = FXMLLoader.load(Jeu.class.getResource("pageAuthentification.fxml"));
+        //Scene scene = new Scene(authPage);
+        //Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        //stage.setScene(scene);
+        //stage.show();
 
         boolean canLogIn = checkLogIn();
 
@@ -63,35 +147,119 @@ public class AuthController {
     }
 
     private boolean checkLogIn() throws IOException {
+        //garder le try/catch ?
+        try {
+            Player player = connectionDB.getPlayerByUsername(username.getText().toString());
 
-        Player player = connectionDB.getPlayerByUsername(username.getText().toString());
+            String passwordDb = null;
 
-        String passwordDb = null;
+            if (player != null) {
+                passwordDb = player.getPassword();
+            }
 
-        if(player != null){
-            passwordDb = player.getPassword();
+            if (username.getText().toString().equals(player.getUsername()) && password.getText().toString().equals(passwordDb)) {
+                return true;
+            } else if (username.getText().isEmpty() || password.getText().isEmpty()) {
+                errorMsg.setText("Please enter your username and password");
+                return false;
+            } else {
+                errorMsg.setText("Some of your info isn't correct. Please try again.");
+                return false;
+            }
         }
-
-        if(username.getText().toString().equals(player.getUsername()) && password.getText().toString().equals(passwordDb)){
-            return true;
+        catch (Exception e) {
+            e.printStackTrace();
         }
-        else if(username.getText().isEmpty() || password.getText().isEmpty()){
-            errorMsg.setText("Please enter your username and password");
-            return false;
-        }
-        else{
-            errorMsg.setText("Some of your info isn't correct. Please try again.");
-            return false;
-        }
+        return false;
     }
 
     @FXML
     public void userSignIn(ActionEvent e) throws IOException {
-        AnchorPane authPage = FXMLLoader.load(Jeu.class.getResource("pageAuthentification.fxml"));
-        Scene scene = new Scene(authPage);
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        boolean signedIn = checkSignIn();
+        if(signedIn){
+            errorMsg.setText("Your account was created. Please log in.");
+            username.setText("");
+            password.setText("");
+        }
+        else{
+            errorMsg.setText("There was an error, please try again.");
+        }
+    }
+
+    private boolean checkSignIn() throws IOException{
+        //garder le try/catch ?
+        try{
+            Player player = connectionDB.getPlayerByUsername(username.getText().toString());
+
+            if (player == null) {
+                boolean playerAdded = connectionDB.addNewPlayer(new Player(0, username.getText().toString(), password.getText().toString(), 0, choicePerso ));
+                return playerAdded;
+            }
+            else if(player != null){
+                errorMsg.setText("This username is already taken.");
+                return false;
+            }
+            else{
+                errorMsg.setText("There was an error, please try again.");
+                return false;
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public void choosePerso(int choice){
+        //ajouter choosePerso sur les boutons dans le fichier fxml en format choosePerso(0) choosePerso(1)...
+        switch (choice){
+            case 0:
+                if(getChoicePerso() != 0){
+                    imageZero.setStyle("-fx-background-color: none;" +
+                            " -fx-border-style: solid inside;" +
+                            "-fx-border-width: 2; " +
+                            "-fx-border-radius: 5; " +
+                            "-fx-border-color:  #21EF80;");
+                    imageOne.setStyle("-fx-background-color: none;");
+                    imageTwo.setStyle("-fx-background-color: none;");
+                    imageThree.setStyle("-fx-background-color: none;");
+                }
+                //-fx-background-color: none;
+                break;
+            case 1:
+                imageOne.setStyle("-fx-background-color: none;" +
+                        " -fx-border-style: solid inside;" +
+                        "-fx-border-width: 2; " +
+                        "-fx-border-radius: 5; " +
+                        "-fx-border-color:  #21EF80;");
+                imageZero.setStyle("-fx-background-color: none;");
+                imageTwo.setStyle("-fx-background-color: none;");
+                imageThree.setStyle("-fx-background-color: none;");
+                break;
+            case 2:
+                imageTwo.setStyle("-fx-background-color: none;" +
+                        " -fx-border-style: solid inside;" +
+                        "-fx-border-width: 2; " +
+                        "-fx-border-radius: 5; " +
+                        "-fx-border-color:  #21EF80;");
+                imageZero.setStyle("-fx-background-color: none;");
+                imageOne.setStyle("-fx-background-color: none;");
+                imageThree.setStyle("-fx-background-color: none;");
+                break;
+            case 3:
+                imageThree.setStyle("-fx-background-color: none;" +
+                        " -fx-border-style: solid inside;" +
+                        "-fx-border-width: 2; " +
+                        "-fx-border-radius: 5; " +
+                        "-fx-border-color:  #21EF80;");
+                imageZero.setStyle("-fx-background-color: none;");
+                imageOne.setStyle("-fx-background-color: none;");
+                imageTwo.setStyle("-fx-background-color: none;");
+                break;
+        }
+        setChoicePerso(choice);
     }
 
 }
--- src/main/java/application/connectionDB.java
+++ src/main/java/application/connectionDB.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class connectionDB {
@@ -25,7 +26,7 @@ public class connectionDB {
         ResultSet result = null;
 
 
-        String queryDb = "SELECT * FROM \"UTILISATEUR\" WHERE USERNAME = \""+ _username +"\" ";
+        String queryDb = MessageFormat.format("SELECT * FROM \"UTILISATEUR\" WHERE USERNAME = \"{0}\" ", _username);
 
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -34,11 +35,11 @@ public class connectionDB {
             result = statement.executeQuery();
 
             if(result != null){
-                int id = Integer.parseInt(result.getString("id"));
+                int id = result.getInt("id");
                 String username = result.getString("username");
                 String password = result.getString("password");
-                int score = Integer.parseInt(result.getString("score"));
-                int perso = Integer.parseInt(result.getString("perso"));
+                int score = result.getInt("score");
+                int perso = result.getInt("perso");
 
                 Player player = new Player(id, username, password, score, perso);
                 return player;
@@ -58,5 +59,43 @@ public class connectionDB {
         }
 
         return null;
+    }
+
+    public static boolean addNewPlayer(Player player) {
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        //score à supprimer -> when newPlayer, score default 0 in DB
+        //password à modifier pour hashage
+        String queryDb = MessageFormat.format("INSERT INTO UTILISATEUR (username, password, score, perso) VALUES (''{0}'', ''{1}'', 0, {2})", player.getUsername(), player.getPassword(), player.getPerso());
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection(url, username, password);
+            statement = conn.prepareStatement(queryDb);
+            result = statement.executeQuery();
+
+            // à tester :
+            // getStatement()
+            // Retrieves the Statement object that produced this ResultSet object.
+
+            if(result != null){
+                return true;
+                //Player player = new Player(id, username, password, score, perso);
+            }
+            else{
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (result != null) { try { result.close(); } catch (SQLException e) {} result = null; }
+            if (statement != null) { try { statement.close(); } catch (SQLException e) {} statement = null; }
+            if (conn != null) { try { conn.close(); } catch (SQLException e) {} conn = null; }
+        }
+        return false;
     }
 }
