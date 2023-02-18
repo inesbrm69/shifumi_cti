4a15eb6b3331b1807303c220acc5e9948b8c1bd2
inesb <bourhim.ines@gmail.com>
2023-02-18 18:04:46 +0100

    Modification interface graphique

--- ProjetJava.sql
+++ ProjetJava.sql
@@ -4,6 +4,7 @@ INCREMENT BY 1;
 
 DROP SEQUENCE auto_increment_user;
 
+--Auto-incrementation des users
 CREATE OR REPLACE TRIGGER auto_increment_user
 BEFORE INSERT ON UTILISATEUR
 FOR EACH ROW
@@ -11,4 +12,13 @@ BEGIN
     select auto_increment_user.nextval INTO :new.id FROM DUAL;
 END;
 
+--Ajout d'un utilisateur (premier utilisateur)
 INSERT INTO UTILISATEUR (username, password, perso, score) VALUES ('admin','password', 2, 0);
+
+--Initialisation des perso au perso 0 (la blonde)
+ALTER TABLE utilisateur
+MODIFY perso DEFAULT 0;
+
+--Initialisation des scores à 0
+ALTER TABLE utilisateur
+MODIFY score DEFAULT 0;
