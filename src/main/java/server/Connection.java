d92fac08ab68e8ae41d891ce33e67f6568a300a4
KCRitoki <tom.duclos@outlook.fr>
2023-02-26 19:06:42 +0100

    Préviens si le joueur est déjà connecté

--- src/main/java/server/Connection.java
+++ src/main/java/server/Connection.java
@@ -9,10 +9,12 @@ import common.PlayerSingleton;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Connection implements Runnable{
 	private Server server;
 	private ServerSocket serverSocket;
+	private String[] connectedUsers = {}; // Tableau de String vide
 
 	public Connection(Server server) {
 		super();
@@ -34,14 +36,22 @@ public class Connection implements Runnable{
 				ConnectedClient newClient = new ConnectedClient(server, sockNewClient);
 				newClient.setId(server.getNumClients());
 
-				if(server.getPlayingClients().size() < 8) {
-					server.addClient(newClient);
-					server.addPlayingClient(newClient);
-				}
-				else {
-					server.addClient(newClient);
-					server.addWaitingClients(newClient);
-					server.sendMessageToId(new Message("Trop de joueurs connectés..."), newClient.getId());
+				String user = newClient.getPlayerUsername();
+				if (connectedUsers.length == 0 || !Arrays.asList(connectedUsers).contains(user)) {
+					if(server.getPlayingClients().size() < 8) {
+						server.addClient(newClient);
+						server.addPlayingClient(newClient);
+						connectedUsers = Arrays.copyOf(connectedUsers, connectedUsers.length + 1);
+						connectedUsers[connectedUsers.length - 1] = user;
+						System.out.println("Joueur ajouté dans le tableau");
+					}
+					else {
+						server.addClient(newClient);
+						server.addWaitingClients(newClient);
+						server.sendMessageToId(new Message("Trop de joueurs connectés..."), newClient.getId());
+					}
+				} else {
+					System.out.println("Joueur déjà dans le tableau");
 				}
 
 				Thread threadNewClient = new Thread(newClient);
